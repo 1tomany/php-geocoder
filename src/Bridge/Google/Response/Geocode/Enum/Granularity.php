fbf9412af7797f98f7e93f679de87fdb2bccbce4
Vic Cherubini <vcherubini@gmail.com>
2026-08-22 00:20:02 -0500

    wip

--- src/Bridge/Google/Response/Geocode/Enum/Granularity.php
+++ src/Bridge/Google/Response/Geocode/Enum/Granularity.php
@@ -4,20 +4,20 @@ namespace OneToMany\Geocoder\Bridge\Google\Response\Geocode\Enum;
 
 enum Granularity: string
 {
-    case Approximate = 'APPROXIMATE';
-    case GeometricCenter = 'GEOMETRIC_CENTER';
-    case RangeInterpolated = 'RANGE_INTERPOLATED';
     case Rooftop = 'ROOFTOP';
-    case Unspecified = 'GRANULARITY_UNSPECIFIED';
+    case RangeInterpolated = 'RANGE_INTERPOLATED';
+    case GeometricCenter = 'GEOMETRIC_CENTER';
+    case Approximate = 'APPROXIMATE';
+    case GranularityUnspecified = 'GRANULARITY_UNSPECIFIED';
 
     public function getAccuracy(): float
     {
         return match ($this) {
-            self::Approximate => 0.3,
-            self::GeometricCenter => 0.5,
-            self::RangeInterpolated => 0.7,
-            self::Rooftop => 0.99,
-            self::Unspecified => 0.0,
+            self::Rooftop => 0.99, // 1
+            self::RangeInterpolated => 0.7, // 25
+            self::GeometricCenter => 0.5, // 250
+            self::Approximate => 0.3, // 1000
+            self::GranularityUnspecified => 0.0, // -1
         };
     }
 }
