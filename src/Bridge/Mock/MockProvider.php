0b491df5c2da7b3d9715431812f8a6c78eea0283
Vic Cherubini <vcherubini@gmail.com>
2026-08-26 15:38:04 -0500

    wip

--- src/Bridge/Mock/MockProvider.php
+++ src/Bridge/Mock/MockProvider.php
@@ -87,7 +87,11 @@ final readonly class MockProvider implements ProviderInterface
 
     private function getGranularity(): string
     {
-        /** @return non-empty-string */
-        return $this->faker->randomElement(['rooftop', 'nearby', 'approximate', 'unknown']);
+        /**
+         * @var non-empty-string $granularity
+         */
+        $granularity = $this->faker->randomElement(['rooftop', 'nearby', 'approximate', 'unknown']);
+
+        return $granularity;
     }
 }
