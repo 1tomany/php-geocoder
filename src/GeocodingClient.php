7c42bbf34bed07efd3957fc12d584453a1d533e9
Vic Cherubini <vcherubini@gmail.com>
2026-09-02 01:08:55 -0500

    wip

--- src/GeocodingClient.php
+++ src/GeocodingClient.php
@@ -28,8 +28,10 @@ final readonly class GeocodingClient implements GeocodingClientInterface
      * @see OneToMany\Geocoder\Contract\GeocodingClientInterface
      */
     #[\Override]
-    public function forward(string|GeocodingVendor $vendor, ForwardGeocode $request): Response
-    {
+    public function forward(
+        string|GeocodingVendor $vendor,
+        ForwardGeocode $request,
+    ): Response {
         return $this->providers->get(GeocodingVendor::create($vendor))->forward($request);
     }
 
@@ -37,8 +39,25 @@ final readonly class GeocodingClient implements GeocodingClientInterface
      * @see OneToMany\Geocoder\Contract\GeocodingClientInterface
      */
     #[\Override]
-    public function reverse(string|GeocodingVendor $vendor, ReverseGeocode $request): Response
-    {
+    public function reverse(
+        string|GeocodingVendor $vendor,
+        ReverseGeocode $request,
+    ): Response {
         return $this->providers->get(GeocodingVendor::create($vendor))->reverse($request);
+    }
+
+    /**
+     * @see OneToMany\Geocoder\Contract\GeocodingClientInterface
+     */
+    #[\Override]
+    public function request(
+        string|GeocodingVendor $vendor,
+        ForwardGeocode|ReverseGeocode $request,
+    ): Response {
+        if ($request instanceof ForwardGeocode) {
+            return $this->forward($vendor, $request);
+        }
+
+        return $this->reverse($vendor, $request);
     }
 }
