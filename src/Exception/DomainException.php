efd9ba9e86a3e5ef7fc4b8c8fe46b7422793e181
Vic Cherubini <vcherubini@gmail.com>
2026-09-02 00:21:12 -0500

    wip

--- src/Exception/DomainException.php
+++ src/Exception/DomainException.php
@@ -4,6 +4,6 @@ namespace OneToMany\Geocoder\Exception;
 
 use OneToMany\Geocoder\Contract\Exception\ExceptionInterface;
 
-final class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
+final class DomainException extends \DomainException implements ExceptionInterface
 {
 }
