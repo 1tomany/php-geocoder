5d6736d99714be8f1d900315246d3f2e186c201d
Vic Cherubini <vcherubini@gmail.com>
2026-09-02 00:32:14 -0500

    wip

--- src/Resource/FowardGeocode.php
+++ src/Resource/FowardGeocode.php
@@ -3,7 +3,9 @@
 namespace OneToMany\Geocoder\Resource;
 
 use OneToMany\Geocoder\Exception\DomainException;
+use OneToMany\Geocoder\Exception\RuntimeException;
 
+use function is_int;
 use function is_string;
 use function trim;
 use function vsprintf;
@@ -49,10 +51,16 @@ final class FowardGeocode
         }
     }
 
+    /**
+     * @var non-empty-string
+     */
     public string $line {
         get => $this->createLine();
     }
 
+    /**
+     * @return non-empty-string
+     */
     private function createLine(): string
     {
         $line = vsprintf('%s %s %s', [
@@ -61,6 +69,10 @@ final class FowardGeocode
             trim((string) $this->unit),
         ]);
 
-        return trim($line);
+        if ('' === $line = trim($line)) {
+            throw new RuntimeException('An empty line was created.');
+        }
+
+        return $line;
     }
 }
